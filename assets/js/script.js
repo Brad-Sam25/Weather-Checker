cf3d9ebd9c19169dc1ae15335093d725d381e15d
Brad Samuel <bradsamuel97@gmail.com>
2021-05-23 20:00:20 -0400

    saving changes to html and addition of js file

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let apiKey = "&appid=f42d672fd7526af9968448c78ac08c13";
+
+let searchBtn = document.querySelector("#searchBtn");
+let userInput = document.querySelector("#user-input");
+let ulEl = document.querySelector("#ul-list");
+let cityName = document.querySelector("#city-name");
+let currentDay = document.querySelector("#today");
+let temp = document.querySelector("#temperature");
+let humdity = document.querySelector("#humidity");
+let UVIndex = document.querySelector("#uv-index");
